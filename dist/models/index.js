c509e91b8304cba5763892f18420c5e16f41278c
fjplaurr <fjplaurr@gmail.com>
2020-12-22 10:33:00 +0100

    deleted dist folder

--- dist/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Post = exports.User = void 0;
-const user_1 = require("./user");
-exports.User = user_1.default;
-const post_1 = require("./post");
-exports.Post = post_1.default;
-//# sourceMappingURL=index.js.map
