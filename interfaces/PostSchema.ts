d80c9684567eeb1895c4f2b3811d57524ef82b18
fjplaurr <fjplaurr@gmail.com>
2020-12-22 10:45:34 +0100

    deleted mongoose schemas to be able to include typegoose

--- interfaces/PostSchema.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-import { UserSchema } from '.';
-
-export interface PostSchema extends Document {
-  _id: string,
-  text: string,
-  date: Date,
-  author: UserSchema['id'],
-}
