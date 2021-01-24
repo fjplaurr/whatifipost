4aa85ba5e13ba05febea769f1e8dc1ecdeb0de9e
fjplaurr <fjplaurr@gmail.com>
2021-01-24 11:38:01 +0100

    Add headers for authorization in fetching

--- client/src/helpers/fetch.ts
+++ client/src/helpers/fetch.ts
@@ -1,18 +1,23 @@
 // Gets generic data
-const get = async (url: string) => {
-  const response = await fetch(url);
+const get = async (url: string, headers = {}) => {
+  const request = {
+    method: 'GET',
+    headers,
+  };
+  const response = await fetch(url, request);
   const json = await response.json();
   return json;
 };
 
 // Posts generic data
-const post = async (url: string, data: {}) => {
+const post = async (url: string, data: {}, headers = {}) => {
   const body = JSON.stringify(data);
   const request = {
     method: 'POST',
     body,
     headers: {
       'Content-Type': 'application/json',
+      ...headers,
     },
   };
   const response = await fetch(url, request);
@@ -21,13 +26,14 @@ const post = async (url: string, data: {}) => {
 };
 
 // Puts generic data
-const put = async (url: string, data: {}) => {
+const put = async (url: string, data: {}, headers = {}) => {
   const body = JSON.stringify(data);
   const request = {
     method: 'PUT',
     body,
     headers: {
       'Content-Type': 'application/json',
+      ...headers,
     },
   };
   const response = await fetch(url, request);
@@ -36,9 +42,10 @@ const put = async (url: string, data: {}) => {
 };
 
 // Deletes single file by id
-const deleteById = async (url: string) => {
+const deleteById = async (url: string, headers = {}) => {
   const request = {
     method: 'DELETE',
+    headers,
   };
   const response = await fetch(url, request);
   const json = await response.json();
@@ -46,12 +53,13 @@ const deleteById = async (url: string) => {
 };
 
 // Posts a file using FormData
-const postFile = async (url: string, file: File) => {
+const postFile = async (url: string, file: File, headers = {}) => {
   const formData = new FormData();
   formData.append('image', file);
   const request = {
     method: 'POST',
     body: formData,
+    headers,
   };
   const response = await fetch(url, request);
   const json = await response.json();
