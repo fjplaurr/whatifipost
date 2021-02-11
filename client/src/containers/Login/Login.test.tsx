012f096ee77ab30f1b92bded2aec5d14e15b9b75
fjplaurr <fjplaurr@gmail.com>
2021-02-11 14:52:17 +0100

    Delete Jest files to use only Cypress

--- client/src/containers/Login/Login.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import Login from './Login.component';
-
-test('Renders correctly', () => {
-  render(<Login />);
-  expect(screen.getByRole('button', { name: /join/i })).toBeInTheDocument();
-  expect(screen.getByRole('button', { name: /sign in/i })).toBeInTheDocument();
-});
--- client/src/containers/NotFound/NotFound.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import NotFound from './NotFound.component';
-
-test('Render not found', () => {
-  render(<NotFound />);
-  expect(screen.getByRole('heading')).toHaveTextContent(/404/i);
-});
